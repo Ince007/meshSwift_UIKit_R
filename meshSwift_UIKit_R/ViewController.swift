36cf6f5a221a08b133c96a89af4b72c35a02373a
Ince007 <tommy2-991@hotmail.it>
2020-06-13 17:44:28 +0200

    Initial Commit

---
+++ meshSwift_UIKit_R/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meshSwift_UIKit_R
+//
+//  Created by Vincenzo Manno on 13/06/2020.
+//  Copyright © 2020 Vincenzo Manno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
